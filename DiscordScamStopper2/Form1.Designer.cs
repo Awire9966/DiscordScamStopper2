31a95505412e37429b3e0119d3203ddac3a8651f
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-07-22 10:19:25 -0400

    Add files via upload

---
+++ DiscordScamStopper2/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DiscordScamStopper2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Website = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select what website you think it might be.";
+            // 
+            // Website
+            // 
+            this.Website.FormattingEnabled = true;
+            this.Website.Items.AddRange(new object[] {
+            "Discord",
+            "Roblox",
+            "Apple",
+            "Steam",
+            "Github",
+            "Instagram",
+            "Twitter",
+            "Facebook"});
+            this.Website.Location = new System.Drawing.Point(55, 68);
+            this.Website.Name = "Website";
+            this.Website.Size = new System.Drawing.Size(666, 21);
+            this.Website.TabIndex = 1;
+            this.Website.Text = "(Select Here)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(634, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Is it the real Website?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(55, 138);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(575, 23);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter In The Website they sent you.";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(448, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(333, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Submit this info To The Awire Scam Database to help other users";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(636, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Paste ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 264);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Website);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "DiscordScamStopper 2 | Stop Discord Scams right in its tracks.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Website;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DiscordScamStopper2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscordScamStopper2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string Compare(ComboBox cb)
+        {
+            if (cb.Text == "Discord")
+            {
+                return "https://discord";
+            }
+            if (cb.Text == "Apple")
+            {
+                return "https://apple.com";
+            }
+            if (cb.Text == "Roblox")
+            {
+                return "https://roblox.com";
+            }
+            if (cb.Text == "Steam")
+            {
+                return "steampowered.com";
+            }
+            if (cb.Text == "Github")
+            {
+                return "https://github.com";
+            }
+            if (cb.Text == "Instagram")
+            {
+                return "https://instagram.com";
+            }
+            if (cb.Text == "Twitter")
+            {
+                return "https://twitter.com";
+            }
+            if (cb.Text == "Facebook")
+            {
+                return "https://facebook.com";
+            }
+
+
+            return "idk";
+
+
+        }
+        private bool CompareBothURLs(RichTextBox box, string outsite)
+        {
+            return box.Text.Contains(outsite);
+        }
+
+               //  private void PostData(bool isreal, CheckBox check)
+               //{
+               // if(check.Checked == true)
+               //   {
+               //    if(isreal == false)
+               // {
+                   // var values = new Dictionary<string, string>
+                   // {
+                   //   { "NotImplemtmented" }
+                   // };
+
+                   // var content = new FormUrlEncodedContent(values);
+
+                   // var response =  client.PostAsync("awiresoftware.xyz:6900/postscam", content);
+
+                    // var responseString = response.Content.ReadAsStringAsync();
+                // }
+               // else
+                //{
+                    //return;
+               /// }
+            //}
+         //else
+            //{
+              //  return;
+           // }
+       // }
+    }
+}
